54f013bc72d7647c8432393c14878fd89dce4ae3
shivani <22251a1235@gmail.com>
2025-09-01 14:32:37 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,8 @@
 public class Test {
   public static void main(String args[]) {
     try {
-      if (Fact.compute(5)!=120) throws AssertionError("Test input failed for 5");
-      if (Fact.compute(0)!=1) throws AssertionError("Test input failed for 0");
+      if (Fact.compute(5)!=120) throws new AssertionError("Test input failed for 5");
+      if (Fact.compute(0)!=1) throws new AssertionError("Test input failed for 0");
       System.out.println("All test cases passed");
     }
     catch(Exception e) {
